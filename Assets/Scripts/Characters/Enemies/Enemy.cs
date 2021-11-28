3304058f8807a917944f7bdfb414dbec57683fe1
Allen-Slaughter <969314823@qq.com>
2021-11-28 11:19:35 +0800

    Dynamic score display and Bug Fix

--- Assets/Scripts/Characters/Enemies/Enemy.cs
+++ Assets/Scripts/Characters/Enemies/Enemy.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Enemy : Character
 {
+    [SerializeField] int scorePoint = 100;
     [SerializeField] int deathEnegryBonus = 3;
 
     public override void Die()
     {
+        ScoreManager.Instance.AddScore(scorePoint);
         PlayerEnergy.Instance.Obtain(deathEnegryBonus);
         EnemyManager.Instance.RemoveFromList(gameObject);
         base.Die();
